9716ecd518c216fb19e806dd0bb755c516ac53e2
Matthew Ellison <mellison@doctorlogic.com>
2018-11-17 22:02:12 -0600

    updated globals

--- functions/branchdescriptions.sh
+++ functions/branchdescriptions.sh
@@ -1,19 +1,30 @@
 
 declare -A branchNames;
-branchNames[test]='hello';
-
 
 #--Local Branch Name helper
 function nameBranch(){
 	branchNames[$1]=$2;
 }
 
+
 function lb(){
+	echo 
 	for K in "${!branchNames[@]}"; do echo -e "$K \t ${branchNames[$K]}";  done 
 }
 
 function nb(){
-	branchNames[$1]=$2
+	branchNames[$1]=$2;
+	rm mattslist
+	for K in "${!branchNames[@]}"; do echo -e "$K,${branchNames[$K]}" >> mattslist;  done 
 }
-
+test='what';
+function loadlist(){
+	cat mattslist | while read line;do
+		key="${line/,*}";
+		value=${line#*,};
+		branchNames[$key]=$value;
+		#  grep -P '\t':w
+		#$line
+	done;
+}
 
